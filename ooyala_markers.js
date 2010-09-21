7017a112f650696a071287c48fa360fd02920c6b
joe <eojthebrave@gmail.com>
2010-09-21 16:55:53 -0500

    Fix issue where videos started playing automatically even if no chapter was specified.
    Documentation cleanup.

--- ooyala_markers.js
+++ ooyala_markers.js
@@ -7,7 +7,9 @@ Drupal.behaviors.ooyalaMarkersJumpToAttach = function(context) {
     var fragment = href.slice(href.indexOf('#') + 1).split(':');
 
     player = document.getElementById(fragment[0] + '_ooyala_player');
-    Drupal.ooyalaMarkersJumpTo(player, fragment[1]);
+    if (parseInt(fragment[1])) {
+      Drupal.ooyalaMarkersJumpTo(player, fragment[1]);
+    }
 
     // Set the browser's URL so people can link directly to this chapter.
     window.location = href;
@@ -15,6 +17,15 @@ Drupal.behaviors.ooyalaMarkersJumpToAttach = function(context) {
   });
 }
 
+/**
+ * Tell Ooyala video player to jump to the specified time within the current
+ * video.
+ *
+ * @param player
+ *   Player DOM object.
+ * @param time
+ *   Time in milliseconds to jump to.
+ */
 Drupal.ooyalaMarkersJumpTo = function(player, time) {
   // Jump the player to the appropriate time.
   // The time variable is converted to seconds here. Ooyala API claims that
@@ -24,8 +35,13 @@ Drupal.ooyalaMarkersJumpTo = function(player, time) {
   player.playMovie();
 }
 
+// Initialize Drupal.ooyala if it hasn't been yet since it is possible for this
+// module's javascript to be loadded before ooyala_player.js
 Drupal.ooyala = Drupal.ooyala || {'listeners': {}};
 
+/**
+ * Register event listener for ooyala API callbacks.
+ */
 Drupal.ooyala.listeners.ooyalaMarkers = function(player, eventName, p) {
   switch(eventName) {
     case 'apiReady':
@@ -33,7 +49,7 @@ Drupal.ooyala.listeners.ooyalaMarkers = function(player, eventName, p) {
       // video.
       var href = window.location.href;
       var fragment = href.slice(href.indexOf('#') + 1).split(':');
-      if (fragment[0] && fragment[1]) {
+      if (fragment[0] && parseInt(fragment[1])) {
         Drupal.ooyalaMarkersJumpTo(player, fragment[1]);
       }
     break;
